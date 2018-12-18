5b45e41ea040b8eccc0e1d7c7840ec59dd2aaf00
Tõnis Ormisson <tonis@andmemasin.eu>
2018-12-18 08:59:50 +0200

    Init

---
+++ RelevanceImEx.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Tõnis Ormisson <tonis@andmemasin.eu>
+ * @since 3.0.
+ */
+class RelevanceImEx extends PluginBase {
+
+
+    protected $storage = 'DbStorage';
+    static protected $description = 'Import-Export survey relevances';
+    static protected $name = 'Relevance IMEX';
+
+
+    /* Register plugin on events*/
+    public function init() {
+    }
+
+
+
+
+}
