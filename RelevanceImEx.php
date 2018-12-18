9b5990116cf4ff426729cf09e628462301a3259c
Tõnis Ormisson <tonis@andmemasin.eu>
2018-12-18 10:26:34 +0200

    add to beforeToolsMenuRender

--- RelevanceImEx.php
+++ RelevanceImEx.php
@@ -13,6 +13,24 @@ class RelevanceImEx extends PluginBase {
 
     /* Register plugin on events*/
     public function init() {
+        $this->subscribe('beforeToolsMenuRender');
+
+    }
+
+    public function beforeToolsMenuRender() {
+        $event = $this->getEvent();
+
+        /** @var array $menuItems */
+        $menuItems = $event->get('menuItems');
+        $menuItem = new \LimeSurvey\Menu\MenuItem([
+            'label' => self::$name,
+            'icon' =>'',
+            'href' => ''
+        ]);
+        $menuItems[] = $menuItem;
+        $event->set('menuItems', $menuItems);
+        return $menuItems;
+
     }
 
 
