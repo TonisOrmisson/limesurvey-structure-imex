856cc36ae4c6c4c8c2854e2b21ef2fb766ed4cbd
Tõnis Ormisson <tonis@andmemasin.eu>
2025-05-30 13:30:00 +0300

    fix tests?

--- src/views/_footer.php
+++ src/views/_footer.php
@@ -2,7 +2,17 @@
 use tonisormisson\ls\structureimex\StructureImEx;
 
 /** @var StructureImEx $exportPlugin */
-$version = new \SebastianBergmann\Version('1.0.0', $exportPlugin->dir().DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR);
+try {
+    $pluginDir = dirname(dirname(__DIR__)); // Go up two levels from the views directory
+    $version = new \SebastianBergmann\Version('1.0.0', $pluginDir);
+} catch (Exception $e) {
+    // Fallback if there's an issue with the version
+    $version = new class {
+        public function getVersion() {
+            return '1.0.0';
+        }
+    };
+}
 ?>
 <div class="row">
     <div class="pull-right">
