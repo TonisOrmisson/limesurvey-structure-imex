b72fc386481c0c0565bfd30279dbd4f9bd8efef2
Tõnis Ormisson <tonis@andmemasin.eu>
2025-10-30 08:05:59 +0200

    remove LS v2, prepare release 2.1.0

--- src/views/_footer.php
+++ src/views/_footer.php
@@ -2,21 +2,12 @@
 use tonisormisson\ls\structureimex\StructureImEx;
 
 /** @var StructureImEx $exportPlugin */
-try {
     $pluginDir = dirname(dirname(__DIR__)); // Go up two levels from the views directory
     $version = new \SebastianBergmann\Version('1.0.0', $pluginDir);
-} catch (Exception $e) {
-    // Fallback if there's an issue with the version
-    $version = new class {
-        public function getVersion() {
-            return '1.0.0';
-        }
-    };
-}
 ?>
 <div class="row">
     <div class="pull-right">
-        <span class="badge bg-secondary">Version:<?= $version->getVersion();?></span>
+        <span class="badge bg-secondary">Version:<?= $version->asString()?></span>
 
     </div>
 </div>
